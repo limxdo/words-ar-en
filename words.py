33840823fad876d50ee4cab88f6b4517b10920a4
limxdo <233496955+limxdo@users.noreply.github.com>
2025-09-24 22:25:49 +0300

    new version of words.py

--- words.py
+++ words.py
@@ -14,23 +14,43 @@ def clear():
     # 🪟
     elif os.name == 'nt':
         os.system('cls')
+    # error
+    else:
+        print("Unknown clear command for this system.")
 
-def replace_key(dct, old_key, new_key):
+def replace_key(dct, pagenum, old_key, new_key):
     try:
-        dct[new_key] = dct.pop(old_key)
+        dct[pagenum][new_key] = dct[pagenum].pop(old_key)
         return True
     except KeyError:
         return False
+
+def dgt_chk(num):
+    if str(num).isdigit():
+        return str(num)
+    else:
+        return False
+
+# sort a pages numbers
+def sort_numeric_keys(dct): 
+    sorted_data = dict(sorted(dct.items(), key=lambda x: int(x[0])))
+    return sorted_data
 
 # main loop
 while True:
     # user input for mode
     mode = input("choose mode:\n- print / p:\n- add / a\n- remove / rm\n- replace / rp\n\n- clear (clear screen)\n- exit\n>: ").strip().lower()
 
+    if not mode:
+        print('input is empty.')
+        time.sleep(0.5)
+        clear()
+        continue
+
     # check if mode is avilable
-    if mode not in ['print','add','remove','replace','clear','p','a','rm','rp','exit']:
+    if mode not in {'print','add','remove','replace','clear','p','a','rm','rp','exit'}:
         print("invald option.")
-        time.sleep(1)
+        time.sleep(0.5)
         clear()
         continue
 
@@ -48,12 +68,21 @@ while True:
             print("json file has problem!\nfix it please.")
             exit(1)
 
-    # check formated file is correct
+    # load json file and check formated file is correct
     with open(JSON_FILE, 'r', encoding='utf-8') as j:
         try:
             jdb = json.load(j)
+            jdb = sort_numeric_keys(jdb)
         except json.decoder.JSONDecodeError:
             print("json file has problem!\nfix it please.")
+            exit(1)
+
+    with open(JSON_FILE, 'w', encoding='utf-8') as j:
+        try:
+            jdb = sort_numeric_keys(jdb)
+            json.dump(jdb, j, indent=4, ensure_ascii=False)
+        except:
+            print("Unknown Error")
             exit(1)
 
     # print mode
@@ -67,81 +96,239 @@ while True:
         # print file
         else:
             clear()
-            page = 2
-            print("=" * 50, '\n')
-            print(f"\t\t\tPage {page}\n".expandtabs(7))
+            for page in jdb:
+                print("=" * 50, '\n')
+                print(f"\t\t\tPage {page}\n".expandtabs(7))
+                print("=" * 50)
+                for count, kv in enumerate(jdb[page]):
+                    count += 1
+                    print(f"{str(count).zfill(2):<2} - {kv:<5} \t\t\t\t\t\t \t\t\t\t\t\t{jdb[page][kv]}".expandtabs(2))
+                    # print(f"{str(count).zfill(4):<4} - \t{kv:<5} \t\t\t\t====> \t\t\t\t\t\t{jdb[page][kv]}".expandtabs(2))
+            total_words = 0
             print("=" * 50)
-            for count, wordkv in enumerate(jdb):
-                count += 1
-                if (count) % 6 == 0:
-                    page += 1
-                    print("=" * 50, '\n')
-                    print(f"\t\t\tPage {page}\n".expandtabs(7))
-                    print("=" * 50)
-                    print(f"{str(count).zfill(4):<4} - \t{wordkv:<5} \t\t\t\t====> \t\t\t\t\t\t{jdb[wordkv]}".expandtabs(2))
-                    continue
-                else:
-                    print(f"{str(count).zfill(4):<4} - \t{wordkv:<5} \t\t\t\t====> \t\t\t\t\t\t{jdb[wordkv]}".expandtabs(2))
-            print("=" * 50)
-            print(f"Total {page-1} pages.")
+            print(f"Total {len(jdb)} pages.")
+            for i in jdb:
+                for x in jdb[i]:
+                    total_words += 1
+            print(f"Total {total_words} words.")
             input("\npress enter to continue...")
             clear()
             continue
+
     # add mode
     elif mode == 'add' or mode == 'a':
-        # english word
-        enwrd = input("Enter English Word: ").strip().capitalize()
-        # arabic word
-        arwrd = input("Enter Arabic Word: ").strip().capitalize()
+        clear()
+        print("What do you want to add? (1/2)\n\n1 - words\n2 - page\n")
+        porw = input(">: ").strip()
+        bang = 0
+        while porw not in {'1','2'}:
+            porw = input(f"enter '1' or '2' {'!'*bang}: ")
+            bang += 1
+        if porw == '1':
+                # print exist pages
+                print('Exist Pages: ')
+                print(tuple(int(i) for i in jdb))
+                print('\n')
+                # page number
+                print("(You can enter new page number)")
+                page = dgt_chk(input("Enter Page Number: "))
+                bang = 0
+                while not dgt_chk(page):
+                    page = dgt_chk(input(f"Enter Number{'!'*bang}: "))
+                    bang += 1
 
-        # check if word is exist
-        try:
-            # ask if replace if word exist
-            print(f"The English word '{enwrd}' is aleady exist with '{jdb[enwrd]}' Arabic word.\ndo you want replace it with '{arwrd}'? (y/n)")
-            arep = input(": ").strip().lower()
-            # loop if ans not correct
-            while arep not in ['y','n']:
-                print("invlid option.\nplease 'y' or 'n'")
-                arep = input(": ").strip().lower()
-            # replace if ans is yes (y)
-            if arep == 'y':
-                jdb[enwrd] = arwrd
-                print(f"a '{enwrd}' has been replased to '{arwrd}' .")
-            # skip if ans is no (n)
-            elif arep == 'n':
-                print("skipping...")
-                time.sleep(0.5)
+                if page not in jdb:
+                    jdb[page] = {} 
+
+                # english word
+                enwrd = input("Enter English Word: ").strip().capitalize()
+                # check
+                tmp = True
+                bangn = 1
+                while not enwrd:
+                    if tmp:
+                        print("input is empty")
+                        enwrd = input("Please Enter English Word: ").strip().capitalize()
+                    else:
+                        enwrd = input(f"English Word{'!'*bangn}: ")
+                        bangn += 1
+                    tmp = False
+
+                # arabic word
+                arwrd = input("Enter Arabic Word: ").strip().capitalize()
+                # check
+                tmp = True
+                bangn = 1
+                while not arwrd:
+                    if tmp:
+                        print("input is empty")
+                        arwrd = input("Please Enter Arabic Word: ").strip().capitalize()
+                    else:
+                        arwrd = input(f"Arabic Word{'!'*bangn}: ")
+                        bangn += 1
+                    tmp = False
+
+                eexist = False
+                pew = None # 'pew' => page exist word (:
+                for tpage in jdb:
+                    if enwrd in jdb[tpage]:
+                        eexist = True
+                        pew = tpage
+
+                # check if word is exist
+                if eexist:
+                    # if english & arabic word is exist
+                    if jdb[pew][enwrd] == arwrd:
+                        print(f"('{enwrd}': '{arwrd}') is already exist in page {pew}.")
+                        input("\npress enter to continue...")
+                        clear()
+                        continue
+                    # ask if replace it if word is exist
+                    print(f"The English word '{enwrd}' is aleady exist in page {pew} with '{jdb[pew][enwrd]}' Arabic word.\ndo you want replace it with '{arwrd}' in page {pew}? (y/n)")
+                    arep = input(">: ").strip().lower()
+                    # loop if ans not correct
+                    tmp = True
+                    bang = 1
+                    while arep not in {'y','n'}:
+                        if tmp:
+                            print("invlid option.\nplease 'y' or 'n'")
+                            arep = input(">: ").strip().lower()
+                        else:
+                            arep = input(f"'y' or 'n'{'!'*bang}: ").strip().lower()
+                            bang += 1
+                        tmp = False
+                    # replace if ans is yes (y)
+                    if arep == 'y':
+                        jdb[pew][enwrd] = arwrd
+                        print(f"a '{enwrd}' has been replased to '{arwrd}' .")
+                    # skip if ans is no (n)
+                    elif arep == 'n':
+                        print("skipping...")
+                        time.sleep(0.5)
+                        continue
+
+                # if word not exist, add word to dict
+                else:
+                    jdb[page][enwrd] = arwrd
+                # sort keys
+                sort_numeric_keys(jdb)
+                # add new dict to json file with checking
+                try:
+                    with open(JSON_FILE, 'w', encoding='utf-8') as j:
+                        json.dump(jdb, j, indent=4, ensure_ascii=False)
+                except FileNotFoundError:
+                    print(f"Error! file: '{JSON_FILE}' not found.")
+                    exit(1)
+        elif porw == '2':
+            print(f"Exists pages: (dont choose exist page)\n{tuple(i for i in jdb)}\n")
+            npage = input("Enter page number you want do add: ").strip()
+            
+            nbang = 0
+            ebang = 0
+            tmp = True
+            while npage in jdb or not npage.isdigit():
+                if not npage.isdigit():
+                    npage = input(f"enter number{'!'*nbang}: ").strip()
+                    nbang += 1
+                    ebang = 0
+                    tmp = True
+                else:
+                    nbang = 0
+                    if tmp:
+                        print(f"page {npage} already exists, please choose another.")
+                        npage = input(">: ").strip()
+                        nbang = 0
+                        ebang = 0
+                    else:
+                        npage = input(f"enter unexsist page{'!'*ebang}: ").strip()
+                        ebang += 1
+                        if not npage.isdigit():
+                            continue
+                    tmp = False
+
+            jdb[npage] = {}
+            try:
+                jdb = sort_numeric_keys(jdb)
+                with open(JSON_FILE, 'w', encoding='utf-8') as j:
+                    json.dump(jdb, j, indent=4, ensure_ascii=False)
+                print(f"\nPage {npage} has been added to file.")
+                input("\npress enter to continue...")
+                clear()
                 continue
-
-        # if word not exist, add word to dict
-        except KeyError:
-            jdb[enwrd] = arwrd
-        # add new dict to json file with checking
-        try:
-            with open(JSON_FILE, 'w', encoding='utf-8') as j:
-                json.dump(jdb, j, indent=4, ensure_ascii=False)
-            print("seccesful added.")
-        except FileNotFoundError:
-            print(f"Error! file: '{JSON_FILE}' not found.")
-            exit(1)
+            except json.decoder.JSONDecodeError:
+                print("dictionary data has problem!")
+                exit(1)
+            except:
+                print("Unknown Error!")
+                exit(1)
                 
     # replace mode
     elif mode == 'replace' or mode == 'rp':
         clear()
-        print("Words:\n")
-        for i in jdb:
-            print(f"{i} => {jdb[i]}")
+        print("\tWords:\n".expandtabs(11))
+        print('-' * 30)
+        for tpage in jdb:
+            print(f"Page {tpage}:\n")
+            for kv in jdb[tpage]:
+                if not kv:
+                    print("Empty")
+                print(f"{kv}\t{jdb[tpage][kv]}")
+            print('-' * 30,end='')
+            print('\n',end='')
+        
+        # page number
+        bang = 0
+        page = input("\nEnter Page Number: ")
+        while page not in jdb:
+            if not dgt_chk(page):
+                page = input(f"Please Enter Number{'!'*bang}: ")
+                bang += 1
+            else:
+                print(f"page '{page}' not found.")
+                page = input("please enter a exist page number: ")
+                bang = 0
+
+
         # grab replace word from user
         orep = input("\nChoose a word (english) do you want to replace: ").strip().capitalize()
         # if word not found in dict
-        while orep not in jdb:
-            print(f"'{orep}' not found.")
-            orep = input(f"Please enter a exist english word: ").strip().capitalize()
+        ebang = 0
+        nbang = 0
+        while orep not in jdb[page]:
+            if not orep:
+                print(f"input is empty{'!'*ebang}")
+                orep = input(f"Please enter a exist english word: ").strip().capitalize()
+                ebang += 1
+                nbang = 0
+            else:
+                print(f"'{orep}' not found in page {page} {'!'*nbang}")
+                orep = input(f"Please enter a exist english word: ").strip().capitalize()
+                nbang += 1
+                ebang = 0
         # new word to replace with it
-        print(f"replace (English) '{orep}' to: ")
+        print(f"replace (English) '{orep}' in page {page} to: ")
         # leave it
         print("(you can leave it as it is with type: 'LEAVEIT')")
         nrep = input('>: ').strip().capitalize()
+        
+        # # not empty
+        # bang = 0
+        # while not nrep:
+        #     print(f"replace (English) '{orep}' to{'!'*bang}: ")
+        #     nrep = input('>: ').strip().capitalize()
+        #     
+
+        # checking
+        tmp = True
+        bang = 0
+        while not nrep:
+            if tmp:
+                print("input is empty")
+                nrep = input(f"please enter word to replace (English): '{orep}' to: ")
+            else:
+                nrep = (f"replace (English) '{orep}' to{'!'*bang}: ")
+                bang += 1
+            tmp = False
 
         # leave it if LEAVEIT
         if nrep.upper() == 'LEAVEIT':
@@ -151,24 +338,131 @@ while True:
         # replace arabic word
         print(f"Enter a new Arabic word for '{nrep}'")
         # print a old arabic word
-        print(f"old word: {jdb[orep]}")
+        print(f"old word: {jdb[page][orep]}")
         # leave it
         print("(you can leave it as it is with type: 'LEAVEIT')")
         anrep = input('>: ').strip().lower()
 
+        # checking
+        tmp = True
+        bang = 0
+        while not anrep:
+            if tmp:
+                print("input is space")
+                anrep = input(f"please enter a new Arabic word for '{nrep}': ")
+            else:
+                anrep = input(f"arabic word for {nrep}{'!'*bang}")
+                bang += 1
+            tmp = False
+
         # leave it if LEAVEIT
         if anrep.upper() == 'LEAVEIT':
             print(f"OK! leaving it.")
-            anrep = jdb[orep]
+            anrep = jdb[page][orep]
 
-        if not replace_key(jdb, orep, nrep):
+        if not replace_key(jdb, page, orep, nrep):
             print("Unknown Error")
             exit(1)
         else:
-            jdb[nrep] = anrep
+            jdb[page][nrep] = anrep
 
         with open(JSON_FILE, 'w', encoding='utf-8') as j:
             json.dump(jdb, j, indent=4, ensure_ascii=False)
+
+    elif mode == 'remove' or mode == 'rm':
+        # print words & pages
+        clear()
+        print("\tWords / Pages:\n")
+        print('-' * 30)
+        for tpage in jdb:
+            print(f"Page {tpage}:\n")
+            for kv in jdb[tpage]:
+                if not kv:
+                    print("Empty")
+                print(f"{kv}\t{jdb[tpage][kv]}")
+            print('-' * 30,end='')
+            print('\n',end='')
+        # print('-' * 30)
+
+        # choose pages or words
+        print("\nremove pages or words?")
+        print("1 - pages\n2 - words")
+        rmmode = input(">: ").strip().lower()
+        bang = 0
+        while not rmmode in {'1','2'}:
+            rmmode = input(f"please choose (1/2){'!'*bang}: ").strip().lower()
+            bang += 1
+
+        if rmmode == '1':
+            pages = []
+            print("avilable pages: ")
+            for i in jdb:
+                pages.append(i)
+            print(tuple(int(i) for i in pages))
+            dpage = input('\nchoose page you want to delete: ')
+            bang = 0
+            tmp = True
+            while dpage not in set(pages):
+                if tmp:
+                    dpage = input(f"please choose page you want to delete from list: \n{tuple([int(i) for i in pages])}: ")
+                else:
+                    dpage = input(f"choose avilable page{'!'*bang}: ")
+                    bang += 1
+                tmp = False
+            try:
+                del jdb[dpage]
+                with open(JSON_FILE, 'w', encoding='utf-8') as j:
+                    json.dump(jdb, j, indent=4, ensure_ascii=False)
+                print(f"page {dpage} has been removed from file.")
+                input("\npress enter to continue...")
+                clear()
+                continue
+            except json.decoder.JSONDecodeError:
+                print("dictionary data has problem!")
+                exit(1)
+            except:
+                print("Unknown Error!")
+                exit(1)
+
+        elif rmmode == '2':
+            pages = []
+            for i in jdb:
+                pages.append(i)
+            print("Enter page number that contains the word you want to delete")
+            pagetw = input(">: ").strip()
+            bang = 0
+            tmp = True
+            while pagetw not in set(pages):
+                if tmp:
+                    pagetw = input(f"please choose page number contains the word you want to delete from list: \n{tuple([int(i) for i in pages])}: ").strip()
+                else:
+                    pagetw = input(f"choose exist page{'!'*bang}: ").strip()
+                    bang += 1
+                tmp = False
+            pagetwrd = input(f"Enter word do you want delete from page {pagetw} (English): ").strip().capitalize()
+            bang = 0
+            tmp = True
+            while pagetwrd not in jdb[pagetw]:
+                if tmp:
+                    pagetwrd = input(f"please enter an existing word in {pagetw}: ").strip().capitalize()
+                else:
+                    pagetwrd = input(f"enter existing word{'!'*bang}: ").strip().capitalize()
+                    bang += 1
+                tmp = False
+            try:
+                del jdb[pagetw][pagetwrd]
+                with open(JSON_FILE, 'w', encoding='utf-8') as j:
+                    json.dump(jdb, j, indent=4, ensure_ascii=False)
+                print(f"word {pagetwrd} in page {pagetw} has been removed from file.")
+                input("\npress enter to continue...")
+                clear()
+                continue
+            except json.decoder.JSONDecodeError:
+                print("dictionary data has problem!")
+                exit(1)
+            except:
+                print("Unknown Error!")
+                exit(1)
 
     elif mode == 'clear':
         clear()
